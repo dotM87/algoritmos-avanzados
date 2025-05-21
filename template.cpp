b0840c886e62ab4a91675dcb45dfebfa582c0b3e
Miguel Benjamin Zubieta Rios <miguel.zubieta@proton.me>
2025-05-21 12:00:41 -0400

    Refactor template code: add missing typedefs and adjust main function signature

--- template.cpp
+++ template.cpp
@@ -7,8 +7,11 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
+typedef map<int, int> mp;
+typedef priority_queue<int> pq;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define fore(i, a, b) for(int i = (a); i <(b); i++)
 #define trav(a, x) for(auto& a : x)
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
@@ -18,12 +21,13 @@ typedef vector<ll> vll;
 #define se second
 
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 #define DEBUG cerr << "DEBUG: " << __LINE__ << endl;
 
-int main() {
-    FAST_IO;
+const int MOD = 1000000007;
+const ll INF = 9223372036854775807LL;
 
+signed main() {
+    FAST_IO;
 
 
     return 0;
