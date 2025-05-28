261e803231f809ea81977cb45ef6c2be4fe0368b
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2025-05-28 00:18:22 -0400

    Agregar soluciones a los problemas A_Elephant, A_Soldier_and_Bananas, A_Word y B_Periodic_Search (partial solution)

---
+++ training/codeforces/A_Elephant.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mp;
+typedef priority_queue<int> pq;
+
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define fore(i, a, b) for(int i = (a); i <(b); i++)
+#define forv(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define DEBUG cerr << "DEBUG: " << __LINE__ << endl;
+
+const int MOD = 1000000007;
+const ll INF = 9223372036854775807LL;
+
+signed main() {
+  FAST_IO;
+  int n, res;
+  cin >> n;
+  res = 0;
+  if (n>=5){
+    res += n%5==0 ? n/5 : (n/5)+1;
+  }else{
+    res = 1;
+  }
+  cout << res;
+
+  return 0;
+}
