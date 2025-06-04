de6e5df57f76d82a2c40ad71e37eab8f206dc38c
Miguel Benjamin Zubieta Rios <mbenjaminzr@gmail.com>
2025-06-03 23:31:21 -0400

    add solutions for A_In_Search_of_an_Easy_Problem and C_Registration_system

---
+++ training/codeforces/A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int, int> mp;
+typedef priority_queue<int> pq;
+
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define fore(i, a, b) for(int i = (a); i <(b); i++)
+#define forv(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define DEBUG cerr << "DEBUG: " << __LINE__ << endl;
+
+const int MOD = 1000000007;
+const ll INF = 9223372036854775807LL;
+
+signed main() {
+  FAST_IO;
+  int n; cin >> n;
+  bool easy = true;
+  while (n--){
+    char c; cin >> c;
+    if(c != '0') {
+      easy = false;
+      break;
+    }
+  }
+  if (easy) {
+    cout << "EASY" << endl;
+  } else {
+    cout << "HARD" << endl;
+  }
+
+  return 0;
+}
